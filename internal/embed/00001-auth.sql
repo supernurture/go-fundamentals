bf312e579c27181959712ebcb6ff530fdd44a62c
Yonathan Dawang Jati <yonathandawang111@gmail.com>
2026-09-17 19:09:27 +0700

    learn: postgreSQL data types, how to create table and manipulate column

--- internal/embed/00001-auth.sql
+++ internal/embed/00001-auth.sql
@@ -1,5 +1,16 @@
+-- PostgreSQL
+
 CREATE TABLE IF NOT EXISTS users (
     user_id     SERIAL PRIMARY KEY,
     name        VARCHAR(50),
     age         INTEGER
 );
+
+-- Used to add a new column.
+ALTER TABLE users ADD       COLUMN place_of_birth TEXT;
+
+-- Used to change the data type of a column.
+ALTER TABLE users ALTER     COLUMN place_of_birth TYPE VARCHAR(100);
+
+-- Used to delete a column.
+ALTER TABLE users DROP      COLUMN place_of_birth;
